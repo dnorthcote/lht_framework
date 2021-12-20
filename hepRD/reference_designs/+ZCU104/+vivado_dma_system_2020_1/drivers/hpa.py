bebe76b723318e556407552301fe1c91dcad6376
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-12-20 14:42:44 +0000

    Final 1280x720 model

--- hepRD/reference_designs/+ZCU104/+vivado_dma_system_2020_1/drivers/hpa.py
+++ hepRD/reference_designs/+ZCU104/+vivado_dma_system_2020_1/drivers/hpa.py
@@ -2,37 +2,31 @@
 from pynq import DefaultIP
 
 class HoughPerformance(DefaultIP):
-    """Driver for Hough Performance Analyser core logic IP.
-    """
+    """Driver for Hough Performance Analyser core logic IP."""
     
     def __init__(self, description):
         """Construct a HoughPerformance object for
-        controlling the HPA IP core.
-        """
+        controlling the HPA IP core."""
         super().__init__(description=description)
     
     @property
     def reset(self):
-        """Returns the reset register value.
-        """
+        """Returns the reset register value."""
         return self.read(256)
     
     @reset.setter
     def reset(self, reset):
-        """Sets the reset register value.
-        """
+        """Sets the reset register value."""
         self.write(256, reset)
         
     @property
     def clock_cycles(self):
-        """Returns the clock_cycles register value.
-        """
+        """Returns the clock_cycles register value."""
         return self.read(260)
         
     @property
     def overflow(self):
-        """Returns the overflow register value.
-        """
+        """Returns the overflow register value."""
         return self.read(264)
     
     bindto = ['xilinx.com:ip:hpa_module:1.2']

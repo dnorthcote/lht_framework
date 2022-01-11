fe3e9a211403925ebae939c59cd951f6897816b5
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2022-01-11 11:40:01 +0000

    Delete models/lht_640_480 directory

--- models/lht_640_480/stop_parallel_hough_accumulator.m
+++
@@ -1,9 +0,0 @@
-%Get the maximum Rho
-maxRho = ceil(sqrt((height/2)^2+(width/2)^2));
-
-%Get theta and rho
-theta = 0:dTheta:180-dTheta;
-
-A = reshape(out.simout, [maxRho*2, numel(theta)]);
-
-plot3DHPS(A);

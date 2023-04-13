896696eb4cb47afc13ceecf8d6fe71019a91f69d
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2023-04-13 17:19:46 +0100

    Update hdlcoder_ref_design_customization.m

--- hepRD/reference_designs/+ZCU104/hdlcoder_ref_design_customization.m
+++ hepRD/reference_designs/+ZCU104/hdlcoder_ref_design_customization.m
@@ -1,8 +1,7 @@
 function [rd, boardName] = hdlcoder_ref_design_customization
 % Reference design plugin registration file
 
-rd = {'ZCU104.vivado_dma_system_2020_1.plugin_rd', ...
-    };
+rd = {'ZCU104.vivado_dma_system_2020_1.plugin_rd'};
 
 boardName = 'ZCU104 Development Board';
 
